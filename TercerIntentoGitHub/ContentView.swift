572cb6137a92e4fa5bd48faff9fc1b4cb2b3b53a
Juan <juanggallo@hotmail.com>
2021-11-05 15:32:54 +0100

    added TercerIntentoGitHub

---
+++ TercerIntentoGitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TercerIntentoGitHub
+//
+//  Created by Juan Gallo on 4/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Papa!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TercerIntentoGitHub/TercerIntentoGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TercerIntentoGitHubApp.swift
+//  TercerIntentoGitHub
+//
+//  Created by Juan Gallo on 4/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TercerIntentoGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
